f82d9f9237af5e588aa64a61c9b0ccde35d07f42
K3A101 <keisha.alexander@hva.nl>
2022-10-19 11:18:56 +0200

    Achtergrondkleur verandert

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,16 +1,16 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	3,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	0, //sound 0
+	3,  //sound 1
+	9, //sound 2
+	12, //sound 3
+	15, //sound 4
+	16.5, //sound 5
+	24, //sound 6
+	25.8, //sound 7
+	31, //sound 8
+	32, //sound 9
+	34, // sound10
+	86 //sound 11
 ];
